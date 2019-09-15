43d3ea5d0ce99ccab52a1e75061542fdbe4cbe27
Monica <monicarigi@hotmail.com>
2019-09-15 10:57:38 +0700

    120301

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
 import Bookings from './Bookings'
 import Confirm from './Confirm'
 import Create from './Create'
@@ -12,19 +12,33 @@ import Profile from './Profile'
 import Signup from './Signup'
 
 class Router extends React.Component {
+
+	checkAuth = () => {
+		return localStorage.getItem('token') ? true : false
+	}
+
 	render () {
 		return (
 		<BrowserRouter>
       <Switch>
-				<Route path='/favorites' component={Favorites} />
-        <Route path='/bookings' component={Bookings} />
-        <Route path='/profile' component={Profile} />
-				<Route path='/confirm' component={Confirm} />
+				<Route path='/favorites' render={() => this.checkAuth() ? <Favorites/> : <Redirect to="/login"/>} />
+
+        <Route path='/bookings' render={() => this.checkAuth() ? <Bookings/> : <Redirect to="/"/>} />
+
+        <Route path='/profile' render={() => this.checkAuth() ? <Profile/> : <Redirect to="/"/>}/>
+
+				<Route path='/confirm' render={() => this.checkAuth() ? <Confirm/> : <Redirect to="/"/>} />
+
 				<Route path='/signup' component={Signup} />
-				<Route path='/places' component={Places} />
-				<Route path='/create' component={Create} />
-				<Route path='/place/:id' component={Place} />
-				<Route path='/host' component={Host} />
+
+				<Route path='/places' render={() => this.checkAuth() ? <Places/> : <Redirect to="/"/>} />
+
+				<Route path='/create' render={() => this.checkAuth() ? <Create/> : <Redirect to="/"/>} />
+
+				<Route path='/place/:id' render={() => this.checkAuth() ? <Place/> : <Redirect to="/"/>}/>
+
+				<Route path='/host' render={() => this.checkAuth() ? <Host/> : <Redirect to="/"/>} />
+
         <Route path='/' component={Login} />
       </Switch>
     </BrowserRouter>
